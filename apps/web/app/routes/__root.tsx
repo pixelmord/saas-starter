90ac4106e8a774fa49556640cde0fcf2f27a8d38
Andreas Adam <andreas.adam@parcellab.com>
2025-04-20 00:06:15 +0200

    feat: web app and docs app

---
+++ apps/web/app/routes/__root.tsx
@@ -0,0 +1,49 @@
+import type { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext } from "@tanstack/react-router";
+import { Outlet, ScrollRestoration } from "@tanstack/react-router";
+import { Meta, Scripts } from "@tanstack/start";
+import type * as React from "react";
+
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient;
+}>()({
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "SaaS Starter",
+			},
+		],
+	}),
+	notFoundComponent: () => <div>Route not found</div>,
+	component: RootComponent,
+});
+
+function RootComponent() {
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
+}
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+			</head>
+			<body>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
+}
